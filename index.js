0e222136b6c19b7c5bedd8d45a579cfc9081b277
Thiago Santos <tos.oliveira@gmail.com>
2022-04-09 14:54:23 -0300

    stable with circular queue

--- index.js
+++ index.js
@@ -1,42 +1,34 @@
 const fs = require('fs');
 
-function getQueue() {
-    let first;
-    let last;
-    let size = 0;
+function getCircularQueue(size) {
+    let array = new Array(size);
+    let pos = -1;
+    let first = 0;
+    let length = 0;
     return {
         get length() {
-            return size;
+            return length;
         },
         get peek() {
-            return first.value
+            return array[first];
         },
         unshift() {
-            if (size > 0) {
-                size--;
-                const result = first.value;
-                first = first.next;
-                if (!first) {
-                    last = undefined;
-                }
-                return result;
-            }
+            const result = array[first];
+            first = (first + 1) % size;
+            length--;
+            return result;
         },
         push(value) {
-            if (last) {
-                last = last.next = {
-                    value,
-                }
-            } else first = last = { value };
-            size++;
+            length++;
+            pos = (pos + 1) % size;
+            array[pos] = value;
         }
     }
 }
 
-function mergeParts(array, start, mid, end) {
+function mergeParts(array, start, mid, end, queue) {
     let p1 = start;
     let p2 = mid;
-    const queue = getQueue();
     for (let i = start; i < end; i++) {
         if (i > mid && queue.length === 0) break;
         if (p2 >= end) {
@@ -51,6 +43,10 @@ function mergeParts(array, start, mid, end) {
         }
     }
 
+    if (queue.length > 0) {
+        throw new Error('Something is wrong');
+    }
+
     function assign(i, value) {
         if (i < mid) {
             queue.push(array[i]);
@@ -60,12 +56,12 @@ function mergeParts(array, start, mid, end) {
     }
 }
 
-function countInversions(array, start = 0, end = array.length) {
+function recursiveMergeSort(array, start, end, queue) {
     if (end - start > 2) {
         const mid = Math.floor((end + start) / 2);
-        countInversions(array, start, mid);
-        countInversions(array, mid, end);
-        mergeParts(array, start, mid, end);
+        recursiveMergeSort(array, start, mid, queue);
+        recursiveMergeSort(array, mid, end, queue);
+        mergeParts(array, start, mid, end, queue);
     }
     const lastPos = end - 1;
     if (start < lastPos && array[lastPos] < array[start]) {
@@ -75,31 +71,33 @@ function countInversions(array, start = 0, end = array.length) {
     }
 }
 
-const Benchmark = require('benchmark');
+function mergeSort(array) {
+    return recursiveMergeSort(array, 0, array.length, getCircularQueue(Math.ceil(array.length / 2)));
+}
 
-
-const benchmark = new Benchmark.Suite()
 const bigArray = JSON.parse(fs.readFileSync('./_bcb5c6658381416d19b01bfc1d3993b5_IntegerArray.js'));
+const Benchmark = require('benchmark');
+const benchmark = new Benchmark.Suite()
 benchmark
     .add('Vanilla', () => bigArray.slice().sort((a, b) => a - b))
-    .add('Merge sort', () => countInversions(bigArray.slice()))
+    .add('Merge sort', () => mergeSort(bigArray.slice()))
     .on('cycle', event => console.log(`${event.target}`))
     .on('error', err => console.log(err))
     .run();
-// [
-//     // [5, 4, 3], // 3
-//     // [5, 4, 3, 2], // 6
-//     // [5, 4, 3, 2, 1], // 10
-//     // [2, 1, 3, 2, 1], // 5
-//     // [7, 6, 5, 4, 3, 2, 1], // 21
-//     [7, 6, 5, 4, 3, 2, 1, 7, 6, 5, 4, 3, 2, 1],
-//     bigArray // 2407905288,
-// ].forEach(arr => {
-//     let start = Date.now();
-//     console.log(arr.slice().sort((a, b) => a - b))
-//     console.log(`took ${Date.now() - start}`);
-//     start = Date.now();
-//     console.log(countInversions(arr));
-//     console.log(`took ${Date.now() - start}`);
-//     console.log(arr);
-// });
+[
+    // [5, 4, 3], // 3
+    // [5, 4, 3, 2], // 6
+    // [5, 4, 3, 2, 1], // 10
+    // [2, 1, 3, 2, 1], // 5
+    // [7, 6, 5, 4, 3, 2, 1], // 21
+    [7, 6, 5, 4, 3, 2, 1, 7, 6, 5, 4, 3, 2, 1],
+    bigArray // 2407905288,
+].forEach(arr => {
+    let start = Date.now();
+    console.log(arr.slice().sort((a, b) => a - b))
+    console.log(`took ${Date.now() - start}`);
+    start = Date.now();
+    console.log(mergeSort(arr));
+    console.log(`took ${Date.now() - start}`);
+    console.log(arr);
+});
